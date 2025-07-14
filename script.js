180fd8785ac18d987b5132772630c008373e70ad
fegazani <fernanda.antoniaespinozab@gmail.com>
2025-07-14 15:16:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,199 @@
 // Datos de la malla (cursos, requisitos y a qué abren)
-const cursos = [
-  { id: "curso1", nombre: "Fundamentos de los Alimentos y Nutrición", semestre: "1º", requiere: [], abre: ["curso15"] },
-  { id: "curso2", nombre: "Alimentos", semestre: "1º", requiere: [], abre: ["curso13"] },
-  { id: "curso3", nombre: "Biología Celular", semestre: "1º", requiere: [], abre: ["curso7", "curso8"] },
-  { id: "curso4", nombre: "Química General y Orgánica", semestre: "1º", requiere: [], abre: ["curso6", "curso9"] },
-  { id: "curso5", nombre: "Matemáticas Elemental", semestre: "1º", requiere: [], abre: ["curso6", "curso15"] },
-  { id: "curso6", nombre: "Química de los Alimentos", semestre: "2º", requiere: ["curso4", "curso5"], abre: ["curso12"] },
-  { id: "curso7", nombre: "Microbiología", semestre: "2º", requiere: ["curso3"], abre: ["curso13"] },
-  { id: "curso8", nombre: "Bioquímica Nutricional", semestre: "2º", requiere: ["curso4"], abre: ["curso16"] },
-  { id: "curso9", nombre: "Morfología", semestre: "2º", requiere: ["curso3"], abre: ["curso16"] },
-  { id: "curso10", nombre: "Inglés I", semestre: "2º", requiere: [], abre: ["curso11"] },
-  { id: "curso11", nombre: "Inglés II", semestre: "3º", requiere: ["curso10"], abre: ["curso24"] },
-  { id: "curso12", nombre: "Técnicas Dietéticas", semestre: "3º", requiere: ["curso6"], abre: ["curso20"] },
-  { id: "curso13", nombre: "Fisiopatología General y de Sistemas I", semestre: "3º", requiere: ["curso8", "curso9"], abre: ["curso14", "curso17"] },
-  { id: "curso14", nombre: "Nutrición Humana", semestre: "3º", requiere: ["curso13"], abre: ["curso17", "curso18"] },
-  { id: "curso15", nombre: "Higiene y Control de los Alimentos", semestre: "3º", requiere: ["curso7", "curso2"], abre: ["curso20"] },
-  { id: "curso16", nombre: "Psicología y Educación en Salud", semestre: "3º", requiere: [], abre: ["curso19", "curso21"] },
-  { id: "curso17", nombre: "Salud Pública I", semestre: "3º", requiere: ["curso1", "curso5"], abre: ["curso22"] },
-  { id: "curso18", nombre: "Inglés III", semestre: "4º", requiere: ["curso11"], abre: ["curso23"] },
-  { id: "curso19", nombre: "Bromatología y Tecnología de los Alimentos", semestre: "4º", requiere: ["curso12", "curso15"], abre: ["curso26"] },
-  { id: "curso20", nombre: "Fisiopatología General y de Sistemas II", semestre: "4º", requiere: ["curso13"], abre: ["curso27", "curso28"] },
-  { id: "curso21", nombre: "Salud Pública II", semestre: "4º", requiere: ["curso17", "curso16"], abre: ["curso29", "curso30"] },
-  { id: "curso22", nombre: "Inglés IV", semestre: "5º", requiere: ["curso18"], abre: ["curso33"] },
-  { id: "curso23", nombre: "Razonamiento científico y Tecnología de información", semestre: "4º", requiere: ["curso6"], abre: ["curso31"] }, // Aquí ajusta para que tenga sentido
-  { id: "curso24", nombre: "Educación en Alimentación y Nutrición", semestre: "5º", requiere: ["curso16"], abre: ["curso32"] },
-  { id: "curso25", nombre: "Evaluación Nutricional en el Ciclo de Vital", semestre: "5º", requiere: ["curso20", "curso14"], abre: ["curso34", "curso35", "curso32"] },
-  { id: "curso26", nombre: "Nutrición y Alimentación en el Ciclo de Vital", semestre: "5º", requiere: ["curso20", "curso14"], abre: ["curso34", "curso36"] },
-  { id: "curso27", nombre: "Metodología de la Investigación", semestre: "5º", requiere: ["curso21", "curso14"], abre: ["curso37"] },
-  { id: "curso28", nombre: "Administración y Gestión en Salud", semestre: "5º", requiere: ["curso21", "curso14"], abre: ["curso38"] },
-  { id: "curso29", nombre: "Inglés IV", semestre: "5º", requiere: ["curso22"], abre: ["curso36"] },
-  { id: "curso30", nombre: "Dietoterapia del Adulto y Adulto Mayor", semestre: "6º", requiere: ["curso25", "curso26"], abre: ["curso39"] },
-  { id: "curso31", nombre: "Proyectos de Intervención Alimentario Nutricional", semestre: "6º", requiere: ["curso25", "curso27"], abre: ["curso40"] },
-  { id: "curso32", nombre: "Farmacología en Nutrición", semestre: "6º", requiere: ["curso26", "curso29"], abre: ["curso39"] },
-  { id: "curso33", nombre: "Alimentación Institucional I", semestre: "6º", requiere: ["curso19", "curso26"], abre: ["curso41"] },
-  { id: "curso34", nombre: "Gestión y Calidad I", semestre: "6º", requiere: ["curso28"], abre: ["curso42"] },
-  { id: "curso35", nombre: "Integrado de Nutrición y Alimentación I", semestre: "6º", requiere: ["curso24", "curso25"], abre: ["curso39", "curso43"] },
-  { id: "curso36", nombre: "Dietoterapia Materno e Infantil", semestre: "7º", requiere: ["curso30", "curso35", "curso32"], abre: ["curso44", "curso46"] },
-  { id: "curso37", nombre: "Nutrición, Actividad Física y Deporte", semestre: "7º", requiere: ["curso36"], abre: ["curso47", "curso48", "curso49"] },
-  { id: "curso38", nombre: "Nutrición Comunitaria I", semestre: "7º", requiere: ["curso31", "curso35"], abre: ["curso50", "curso51", "curso52"] },
-  { id: "curso39", nombre: "Alimentación Institucional II", semestre: "7º", requiere: ["curso33"], abre: ["curso53", "curso51"] },
-  { id: "curso40", nombre: "Nutrición Comunitaria II", semestre: "7º", requiere: ["curso38"], abre: ["curso47", "curso48", "curso49"] },
-  { id: "curso41", nombre: "Gestión y Calidad II", semestre: "7º", requiere: ["curso34"], abre: ["curso53"] },
-  { id: "curso42", nombre: "Pensamiento Crítico", semestre: "7º", requiere: ["curso23"], abre: ["curso54"] },
-  { id: "curso43", nombre: "Bioética", semestre: "8º", requiere: ["curso38"], abre: ["curso47", "curso48", "curso49"] },
-  { id: "curso44", nombre: "Innovación Alimentaria", semestre: "8º", requiere: ["curso39", "curso41"], abre: ["curso47", "curso48", "curso49"] },
-  { id: "curso45", nombre: "Integrado de Nutrición y Alimentación II", semestre: "8º", requiere: ["curso36", "curso38", "curso39"], abre: ["curso47", "curso48", "curso49"] },
-  { id: "curso46", nombre: "Responsabilidad Social", semestre: "8º", requiere: ["curso42"], abre: ["curso47", "curso48", "curso49"] },
-  { id: "curso47", nombre: "Internado 1: Clínico Adulto e Infantil", semestre: "9º", requiere: ["curso43", "curso44", "curso45", "curso46"], abre: [] },
-  { id: "curso48", nombre: "Internado 2: Alimentación Institucional e Innovación", semestre: "9º", requiere: ["curso43", "curso44", "curso45", "curso46"], abre: [] },
-  { id: "curso49", nombre: "Internado 3: Nutrición Comunitaria", semestre: "10º", requiere: ["curso43", "curso44", "curso45", "curso46"], abre: [] }
-];
+const cursos = {
+  "Fundamentos de los Alimentos y Nutrición": {
+    prereq: [],
+    abre: ["Salud Pública I"]
+  },
+  "Alimentos": {
+    prereq: [],
+    abre: ["Higiene y Control de los Alimentos"]
+  },
+  "Biología Celular": {
+    prereq: [],
+    abre: ["Microbiología", "Morfología"]
+  },
+  "Química General y Orgánica": {
+    prereq: [],
+    abre: ["Química de los Alimentos", "Bioquímica Nutricional"]
+  },
+  "Matemáticas Elemental": {
+    prereq: [],
+    abre: ["Química de los Alimentos", "Salud Pública I"]
+  },
+  "Habilidades Comunicativas": {
+    prereq: [],
+    abre: ["Razonamiento científico y Tecnología de información"]
+  },
+  "Química de los Alimentos": {
+    prereq: ["Química General y Orgánica", "Matemáticas Elemental"],
+    abre: ["Técnicas Dietéticas"]
+  },
+  "Microbiología": {
+    prereq: ["Biología Celular"],
+    abre: ["Higiene y Control de los Alimentos"]
+  },
+  "Bioquímica Nutricional": {
+    prereq: ["Química General y Orgánica"],
+    abre: ["Fisiopatología General y de Sistemas I"]
+  },
+  "Morfología": {
+    prereq: ["Biología Celular"],
+    abre: ["Fisiopatología General y de Sistemas I"]
+  },
+  "Inglés I": {
+    prereq: [],
+    abre: ["Inglés II"]
+  },
+  "Técnicas Dietéticas": {
+    prereq: ["Química de los Alimentos"],
+    abre: ["Bromatología y Tecnología de los Alimentos"]
+  },
+  "Fisiopatología General y de Sistemas I": {
+    prereq: ["Bioquímica Nutricional", "Morfología"],
+    abre: ["Fisiopatología General y de Sistemas II", "Nutrición Humana"]
+  },
+  "Higiene y Control de los Alimentos": {
+    prereq: ["Microbiología", "Alimentos"],
+    abre: ["Bromatología y Tecnología de los Alimentos"]
+  },
+  "Psicología y Educación en Salud": {
+    prereq: [],
+    abre: ["Educación en Alimentación y Nutrición", "Salud Pública II"]
+  },
+  "Salud Pública I": {
+    prereq: ["Fundamentos de los Alimentos y Nutrición", "Matemáticas Elemental"],
+    abre: ["Salud Pública II"]
+  },
+  "Inglés II": {
+    prereq: ["Inglés I"],
+    abre: ["Inglés III"]
+  },
+  "Bromatología y Tecnología de los Alimentos": {
+    prereq: ["Técnicas Dietéticas", "Higiene y Control de los Alimentos"],
+    abre: ["Alimentación Institucional I"]
+  },
+  "Fisiopatología General y de Sistemas II": {
+    prereq: ["Fisiopatología General y de Sistemas I"],
+    abre: ["Evaluación Nutricional en el Ciclo de Vital", "Nutrición y Alimentación en el Ciclo de Vital"]
+  },
+  "Nutrición Humana": {
+    prereq: ["Fisiopatología General y de Sistemas I"],
+    abre: ["Evaluación Nutricional en el Ciclo de Vital", "Nutrición y Alimentación en el Ciclo de Vital"]
+  },
+  "Salud Pública II": {
+    prereq: ["Salud Pública I", "Psicología y Educación en Salud"],
+    abre: ["Metodología de la Investigación", "Administración y Gestión en Salud"]
+  },
+  "Inglés III": {
+    prereq: ["Inglés II"],
+    abre: ["Inglés IV"]
+  },
+  "Razonamiento científico y Tecnología de información": {
+    prereq: ["Habilidades Comunicativas"],
+    abre: ["Pensamiento Crítico"]
+  },
+  "Educación en Alimentación y Nutrición": {
+    prereq: ["Psicología y Educación en Salud"],
+    abre: ["Integrado de Nutrición y Alimentación I"]
+  },
+  "Evaluación Nutricional en el Ciclo de Vital": {
+    prereq: ["Fisiopatología General y de Sistemas II", "Nutrición Humana"],
+    abre: ["Dietoterapia del Adulto y Adulto Mayor", "Proyectos de Intervención Alimentario Nutricional", "Integrado de Nutrición y Alimentación I"]
+  },
+  "Nutrición y Alimentación en el Ciclo de Vital": {
+    prereq: ["Fisiopatología General y de Sistemas II", "Nutrición Humana"],
+    abre: ["Dietoterapia del Adulto y Adulto Mayor", "Farmacología en Nutrición"]
+  },
+  "Metodología de la Investigación": {
+    prereq: ["Salud Pública II", "Nutrición Humana"],
+    abre: ["Proyectos de Intervención Alimentario Nutricional"]
+  },
+  "Administración y Gestión en Salud": {
+    prereq: ["Salud Pública II", "Nutrición Humana"],
+    abre: ["Gestión y Calidad I"]
+  },
+  "Inglés IV": {
+    prereq: ["Inglés III"],
+    abre: ["Farmacología en Nutrición"]
+  },
+  "Dietoterapia del Adulto y Adulto Mayor": {
+    prereq: ["Evaluación Nutricional en el Ciclo de Vital", "Nutrición y Alimentación en el Ciclo de Vital"],
+    abre: ["Dietoterapia Materno e Infantil"]
+  },
+  "Proyectos de Intervención Alimentario Nutricional": {
+    prereq: ["Evaluación Nutricional en el Ciclo de Vital", "Metodología de la Investigación"],
+    abre: ["Nutrición Comunitaria I"]
+  },
+  "Farmacología en Nutrición": {
+    prereq: ["Nutrición y Alimentación en el Ciclo de Vital", "Inglés IV"],
+    abre: ["Dietoterapia Materno e Infantil"]
+  },
+  "Alimentación Institucional I": {
+    prereq: ["Bromatología y Tecnología de los Alimentos", "Nutrición y Alimentación en el Ciclo de Vital"],
+    abre: ["Alimentación Institucional II"]
+  },
+  "Gestión y Calidad I": {
+    prereq: ["Administración y Gestión en Salud"],
+    abre: ["Gestión y Calidad II"]
+  },
+  "Integrado de Nutrición y Alimentación I": {
+    prereq: ["Educación en Alimentación y Nutrición", "Evaluación Nutricional en el Ciclo de Vital"],
+    abre: ["Dietoterapia Materno e Infantil", "Nutrición Comunitaria I"]
+  },
+  "Dietoterapia Materno e Infantil": {
+    prereq: ["Dietoterapia del Adulto y Adulto Mayor", "Integrado de Nutrición y Alimentación I", "Farmacología en Nutrición"],
+    abre: ["Integrado de Nutrición y Alimentación II", "Nutrición, Actividad Física y Deporte"]
+  },
+  "Nutrición Comunitaria I": {
+    prereq: ["Proyectos de Intervención Alimentario Nutricional", "Integrado de Nutrición y Alimentación I"],
+    abre: ["Nutrición Comunitaria II", "Integrado de Nutrición y Alimentación II", "Bioética"]
+  },
+  "Alimentación Institucional II": {
+    prereq: ["Alimentación Institucional I"],
+    abre: ["Innovación Alimentaria", "Integrado de Nutrición y Alimentación II"]
+  },
+  "Nutrición Comunitaria II": {
+    prereq: ["Nutrición Comunitaria I"],
+    abre: ["Internado 1: Clínico Adulto e Infantil", "Internado 2: Alimentación Institucional e Innovación", "Internado 3: Nutrición Comunitaria"]
+  },
+  "Gestión y Calidad II": {
+    prereq: ["Gestión y Calidad I"],
+    abre: ["Innovación Alimentaria"]
+  },
+  "Pensamiento Crítico": {
+    prereq: ["Razonamiento científico y Tecnología de información"],
+    abre: ["Responsabilidad Social"]
+  },
+  "Bioética": {
+    prereq: ["Nutrición Comunitaria I"],
+    abre: ["Internado 1: Clínico Adulto e Infantil", "Internado 2: Alimentación Institucional e Innovación", "Internado 3: Nutrición Comunitaria"]
+  },
+  "Innovación Alimentaria": {
+    prereq: ["Alimentación Institucional II", "Gestión y Calidad II"],
+    abre: ["Internado 1: Clínico Adulto e Infantil", "Internado 2: Alimentación Institucional e Innovación", "Internado 3: Nutrición Comunitaria"]
+  },
+  "Integrado de Nutrición y Alimentación II": {
+    prereq: ["Dietoterapia Materno e Infantil", "Nutrición Comunitaria I", "Alimentación Institucional II"],
+    abre: ["Internado 1: Clínico Adulto e Infantil", "Internado 2: Alimentación Institucional e Innovación", "Internado 3: Nutrición Comunitaria"]
+  },
+  "Responsabilidad Social": {
+    prereq: ["Pensamiento Crítico"],
+    abre: ["Internado 1: Clínico Adulto e Infantil", "Internado 2: Alimentación Institucional e Innovación", "Internado 3: Nutrición Comunitaria"]
+  },
+  "Nutrición, Actividad Física y Deporte": {
+    prereq: ["Dietoterapia Materno e Infantil"],
+    abre: ["Internado 1: Clínico Adulto e Infantil", "Internado 2: Alimentación Institucional e Innovación", "Internado 3: Nutrición Comunitaria"]
+  },
+  "Internado 1: Clínico Adulto e Infantil": {
+    prereq: ["Integrado de Nutrición y Alimentación II", "Bioética", "Nutrición Comunitaria II", "Nutrición, Actividad Física y Deporte", "Innovación Alimentaria", "Responsabilidad Social"]
+  },
+  "Internado 2: Alimentación Institucional e Innovación": {
+    prereq: ["Integrado de Nutrición y Alimentación II", "Bioética", "Nutrición Comunitaria II", "Nutrición, Actividad Física y Deporte", "Innovación Alimentaria", "Responsabilidad Social"]
+  },
+  "Internado 3: Nutrición Comunitaria": {
+    prereq: ["Integrado de Nutrición y Alimentación II", "Bioética", "Nutrición Comunitaria II", "Nutrición, Actividad Física y Deporte", "Innovación Alimentaria", "Responsabilidad Social"]
+  }
+};
 
 // Estado para llevar control de aprobados y desbloqueados
 const estadoCursos = {};
